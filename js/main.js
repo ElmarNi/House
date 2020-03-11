d80fb0a58b8638273b6b2ab9544374e9b2acecf8
unknown <elmarni@code.edu.az>
2020-03-11 17:12:31 +0400

    -

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
 $(document).ready(function () {
-    $("header #open-menu").click(function (e) { 
+    $("header #open-menu").click(function (e) {
         e.preventDefault();
         $("#menu").css("z-index", "99999").addClass("active");;
         $("body").addClass("overflow-hidden");
     });
-    $("#menu #close-menu").click(function(e){
+    $("#menu #close-menu").click(function (e) {
         e.preventDefault();
         $("#menu").css("z-index", "0").removeClass("active");;
         $("body").removeClass("overflow-hidden");
+    })
+    $("#contact-form form .input-wrapper .custom-input").focus(function (e) {
+        e.preventDefault();
+        for (let i = 0; i < 101; i++) {
+            $(this).next().css("width", `${i}%`)
+        }
+    })
+    $("#contact-form form .input-wrapper .custom-input").focusout(function () {
+        $("#contact-form form .input-wrapper .bottom-line").css("width", `0%`)
     })
 });
